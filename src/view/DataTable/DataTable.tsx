ca586f9877a525c81db415a6d99583ee963080e0
cuiyue <cuiyue.msn@hotmail.com>
2020-09-11 18:03:16 +0800

    测试图标

--- src/view/DataTable/DataTable.tsx
+++ src/view/DataTable/DataTable.tsx
@@ -1,4 +1,5 @@
 import React, { FC, useState } from 'react';
+import message from 'antd/lib/message';
 import RootPanel from '@src/components/RootPanel';
 import { helper } from '@src/utils/helper';
 import { useParams } from 'react-router-dom';
@@ -8,28 +9,33 @@ import LoadingContainer from '@src/containers/Loading';
 interface Prop {}
 
 const DataTable: FC<Prop> = (props) => {
-    const [data, setData] = useState<any>({});
+	const [data, setData] = useState<any>({});
 
-    const { file } = useParams<{ file: string }>();
+	const { file } = useParams<{ file: string }>();
 
-    const { loading, setLoading } = LoadingContainer.useContainer();
+	const { loading, setLoading } = LoadingContainer.useContainer();
 
-    useMount(async () => {
-        setLoading(true);
-        const next = await helper.loadJSON(`public/data/${file}.json`, 'data');
-        setData(next);
-        setLoading(false);
-    });
+	useMount(async () => {
+		setLoading(true);
+		try {
+			const next = await helper.loadJSON(`public/data/${file}.json`, 'data');
+			setData(next);
+		} catch (error) {
+			message.error('读取数据失败');
+		} finally {
+			setLoading(false);
+		}
+	});
 
-    return (
-        <RootPanel loading={loading}>
-            <h1>DataTable类页面</h1>
-            <hr />
-            <div>
-                <label>{data.title}</label>
-            </div>
-        </RootPanel>
-    );
+	return (
+		<RootPanel loading={loading}>
+			<h1>DataTable类页面</h1>
+			<hr />
+			<div>
+				<label>{data.title}</label>
+			</div>
+		</RootPanel>
+	);
 };
 
 export default DataTable;
