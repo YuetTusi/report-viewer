70e830e60a36e13048c6cf5bbb4352abb4839036
cuiyue <cuiyue.msn@hotmail.com>
2020-09-16 14:07:22 +0800

    调整样式

--- src/view/Chat/Chat.tsx
+++ src/view/Chat/Chat.tsx
@@ -1,29 +1,41 @@
 import React, { FC, useState } from 'react';
+import message from 'antd/lib/message';
 import { useParams } from 'react-router-dom';
 import { useMount } from '@src/hooks';
+import LoadingContainer from '@src/containers/Loading';
 import { helper } from '@src/utils/helper';
+import RootPanel from '@src/components/RootPanel';
+import { PanelBox } from '@src/components/styled/BoxStyle';
+import { MainTitle } from '@src/components/styled/StyleWidget';
 
 interface Prop {}
 
 const Chat: FC<Prop> = (props) => {
-    const [data, setData] = useState<any>({});
+	const [data, setData] = useState<any>({});
+	const { file } = useParams<{ file: string }>();
 
-    const { file } = useParams<{ file: string }>();
+	useMount(async () => {
+		setLoading(true);
+		try {
+			const next = await helper.loadJSON(`public/data/${file}.json`, 'data');
+			setData(next);
+		} catch (error) {
+			message.error('读取数据失败');
+		} finally {
+			setLoading(false);
+		}
+	});
 
-    useMount(async () => {
-        const next = await helper.loadJSON(`public/data/${file}.json`, 'data');
-        setData(next);
-    });
+	const { loading, setLoading } = LoadingContainer.useContainer();
 
-    return (
-        <div>
-            <h1>Chat类页面</h1>
-            <hr />
-            <div>
-                <label>{data.title}</label>
-            </div>
-        </div>
-    );
+	return (
+		<RootPanel loading={loading}>
+			<PanelBox>
+				<MainTitle>{data.title ?? ''}</MainTitle>
+			</PanelBox>
+			<PanelBox></PanelBox>
+		</RootPanel>
+	);
 };
 
 export default Chat;
